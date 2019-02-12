f5743caf23bfe575fa92dbacd2ed700feb0ba638
Sachin Raut <sachinrau@cybage.com>
2019-02-12 20:02:40 +0530

    testing xray on circle ci

--- circleci_test.sh
+++ circleci_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sleep 5s
 #Install Python dependencies
 sudo apt-get update
 sudo apt-get install python-dev gettext python3-pip  libmysqlclient-dev
@@ -15,17 +15,17 @@ pip3 install -r requirements.txt
 pip3 install selenium==3.8.0
 pip3 install behave
 pip3 install allure-behave
-pip3 install pyvirtualdisplay
-pip3 install ipdb
-pip3 install xvfbwrapper
-pip3 install nose
+#pip3 install pyvirtualdisplay
+#pip3 install ipdb
+#pip3 install xvfbwrapper
+#pip3 install nose
 
 #Install chrome
-sudo apt-get install libxss1 libappindicator1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
+#sudo apt-get install libxss1 libappindicator1 libindicator7
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome*.deb
 
 #install chrome driver
-yes | wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip && yes | sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+#yes | wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip && yes | sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
 
 mkdir /tmp/artifacts
--- run_test.sh
+++ run_test.sh
@@ -2,18 +2,17 @@
 
 # set Python Path
 export PYTHONPATH=.
-
+sleep 2s
 token=$(curl -H "Content-Type: application/json" -X POST --data '{ "client_id": "7CD4B7151C0049AE9E9D49DDC7C8174E","client_secret": "e8f8a5f710f9425c8399933db53424f683b66d6b3dc46bcb67b8abe9b7915683" }'  https://xray.cloud.xpand-it.com/api/v1/authenticate| tr -d '"')
-
+sleep 5s
 curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token"  "https://xray.cloud.xpand-it.com/api/v1/export/cucumber?keys=DSP-14" >cucumber_scenario
-
+sleep 2s
 unzip cucumber_scenario
-
+sleep 1s
 python3 remove_extra_scenario.py
-
+sleep 1s
 behave xray_test/updated_feature_file.feature --format=cucumber_json:PrettyCucumberJSONFormatter -o report_cucumber.json
-
-echo -e "Completed feature : 01_basic.feature"
+sleep 3s
 
 #run behave tests
 #touch /home/circleci/project/xray_test.txt
