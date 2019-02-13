75e097a5af885ea846a21a58672e3700c17456e0
Sachin Raut <sachinrau@cybage.com>
2019-02-13 18:22:49 +0530

    Add code for demo

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=.
 sleep 2s
 token=$(curl -H "Content-Type: application/json" -X POST --data '{ "client_id": "C19C6BF814F7442EA0DC15A86A3A2800","client_secret": "2354aedeaa47f729fbf0ce4c0b16f3b3a1624f759c7e492ad01339c6b3deb179" }'  https://xray.cloud.xpand-it.com/api/v1/authenticate| tr -d '"')
 sleep 5s
-curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token"  "https://xray.cloud.xpand-it.com/api/v1/export/cucumber?keys=DP-7" >cucumber_scenario
+curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token"  "https://xray.cloud.xpand-it.com/api/v1/export/cucumber?keys=DP-8" >cucumber_scenario
 sleep 5s
 unzip cucumber_scenario
 sleep 1s
