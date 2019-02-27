612c265498c7bce1f493564c518561845f224f18
Sachin Raut <sachinrau@cybage.com>
2019-02-27 11:45:54 +0530

    Trigger circleCI

--- post_test.sh
+++ post_test.sh
@@ -3,4 +3,5 @@ chmod 777 -R ~/project
 token=$(curl -H "Content-Type: application/json" -X POST --data '{ "client_id": "C19C6BF814F7442EA0DC15A86A3A2800","client_secret": "2354aedeaa47f729fbf0ce4c0b16f3b3a1624f759c7e492ad01339c6b3deb179" }'  https://xray.cloud.xpand-it.com/api/v1/authenticate| tr -d '"')
 sleep 5s
 
+
 curl -H "Content-Type: application/json" -X POST -H "Authorization: Bearer $token"  --data @"/home/circleci/project/report_cucumber.json" https://xray.cloud.xpand-it.com/api/v1/import/execution/cucumber
